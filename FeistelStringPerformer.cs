2ee9f63310561feda083a745ad29e25008c2a43a
sv7atoslav <52704744+sv7atoslav@users.noreply.github.com>
2019-07-30 15:07:28 +0300

    add part of soucre code
    add my implement of core of program

---
+++ FeistelStringPerformer.cs
@@ -0,0 +1,105 @@
+﻿namespace WPF_FeistelWith_long_key
+{
+    public class FeistelStringPerformer
+    {
+        public enum ParserMode { left, right, all };
+
+        private UshortNodeFeistel[] pairs;
+
+        public byte Size { get { return (byte)((pairs == null) ? 0 : pairs.Length); } }
+
+        private FeistelStringPerformer(ushort[] data, ParserMode pm)
+        {
+            //if (data == null)return;
+            if (data.Length % 2 == 1) throw new System.ArgumentException("not divide by 2");
+            pairs =new UshortNodeFeistel[(pm== ParserMode.all)?data.Length/2:data.Length];
+            switch (pm)
+            {
+                case (ParserMode.left):
+                    {
+                        for (uint index = 0; index < pairs.Length; index++)
+                            pairs[index] = new UshortNodeFeistel(data[index], '\u263a');// '\u263a' is :-)
+                        return;
+                    }
+                case (ParserMode.right):
+                    {
+                        for (byte index = 0; index < pairs.Length; index++)
+                            pairs[index] = new UshortNodeFeistel('\u263a', data[index]);// '\u263a' is :-)
+                        return;
+                    }
+                case (ParserMode.all):
+                    {
+                        for (byte index = 0; index < pairs.Length; index++)
+                            pairs[index] = new UshortNodeFeistel(data[index * 2], data[index * 2 + 1]);
+                        return;
+                    }
+            }
+        }
+
+
+        public static FeistelStringPerformer UploadString(string message, ParserMode pm) { return new FeistelStringPerformer(BicycleConverter.StringToUshortArray(message), pm); }
+
+        /// <summary>
+        /// subcommand for native user (human)
+        /// </summary>
+        /// <returns>rezult of operations</returns>
+        public string ReadStringFromNumbers(ParserMode pm)
+        {
+            ushort[] numbers = null;
+            switch (pm)
+            {
+                case (ParserMode.left):
+                    {
+                        numbers = new ushort[pairs.Length];
+                        for (uint index = 0; index < pairs.Length; index++)
+                            numbers[index] = pairs[index].left_part;
+                        break;
+                    }
+                case (ParserMode.right):
+                    {
+                        numbers = new ushort[pairs.Length];
+                        for (uint index = 0; index < pairs.Length; index++)
+                            numbers[index] = pairs[index].right_part;
+                        break;
+                    }
+                case (ParserMode.all):
+                    {
+                        numbers = new ushort[pairs.Length * 2];
+                        for (uint index = 0; index < pairs.Length; index++)
+                        {
+                            numbers[index * 2] = pairs[index].left_part;
+                            numbers[index * 2 + 1] = pairs[index].right_part;
+                        }
+                        break;
+                    }
+            }
+            return BicycleConverter.UshortArrayToString(numbers);
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <returns>left parts</returns>
+        public override string ToString() { return ReadStringFromNumbers(ParserMode.all); }
+
+        /// <summary>
+        /// </summary>
+        /// <returns>chinese signs</returns>
+        public string Encoder(byte[] deep)
+        {
+            for (byte index = 0; index < pairs.Length; index++)
+                pairs[index].Encrypt(deep[index]);
+            return ReadStringFromNumbers(ParserMode.all);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>plaintext of secret message</returns>
+        public string Decoder(byte[] deep)
+        {
+            for (byte index = 0; index < pairs.Length; index++)
+                pairs[index].Decrypt(deep[index]);
+            return ReadStringFromNumbers(ParserMode.left);
+        }
+    }
+}
---
+++ UshortNodeFeistel.cs
@@ -0,0 +1,89 @@
+﻿// https://habr.com/ru/post/140404/
+using System;
+
+namespace WPF_FeistelWith_long_key
+{
+    public struct UshortNodeFeistel
+    {
+        /// <summary>when = 0 , end of crypto process</summary>
+        byte undone_rounds;
+        /// <summary>times of call Encrypt</summary>
+        static byte limit_rounds = 0;
+
+        /// <summary>L,R data parts</summary>
+        public ushort left_part { get; private set; }
+        public ushort right_part { get; private set; }
+
+        /// <summary>Function must be volatile by left_part and by undone_rounds</summary>
+        /// <remarks>must be very difficult!</remarks>
+        /// <returns>key for xor</returns>
+        public ushort DifficulCryptoFunction(byte current_round) { return (ushort)((3 * left_part + 8 + 4 * current_round - 3) % ushort.MaxValue); }
+
+        public UshortNodeFeistel(ushort start_left_part, ushort start_right_part)//,//byte deep=8*/)
+        {
+            this = new UshortNodeFeistel();
+            undone_rounds = 0;// = limit_rounds;
+            left_part = start_left_part;
+            right_part = start_right_part;
+        }
+
+        /// <summary>function in functional style</summary>
+        /// <remarks>temp exist because dont allov void f in ternar operator</remarks>
+        /// <param name="encrypt">hide or show text</param>
+        private void Round(bool encrypt) { int temp = (encrypt) ? EncoderRound() : DecoderRound(); }
+
+        /// <summary>
+        /// step for hide text
+        /// </summary>
+        /// <returns>0 (always), for use in ternar operator</returns>
+        private int EncoderRound()
+        {
+            ushort rezult_xor = (ushort)(right_part ^ DifficulCryptoFunction((byte)(limit_rounds - undone_rounds + 1)));
+            if (undone_rounds == 1)// else do r-l replase
+                right_part = rezult_xor;
+            else
+            {
+                right_part = left_part;
+                left_part = rezult_xor;
+            }
+            undone_rounds--;
+            return 0;
+        }
+
+        /// <summary>
+        /// step for show text
+        /// </summary>
+        /// <returns>0 (always), for use in ternar operator</returns>
+        private int DecoderRound()
+        {
+            ushort rezult_xor = (ushort)(right_part ^ DifficulCryptoFunction(undone_rounds));
+            if (undone_rounds == 1)right_part = rezult_xor;// else do r-l replase
+            else
+            {
+                right_part = left_part;
+                left_part = rezult_xor;
+            }
+            undone_rounds--;
+            return 0;
+        }
+
+        /// <summary>
+        /// hide a text
+        /// </summary>
+        /// <param name="deep">count of rounds for hide a text</param>
+        public void Encrypt(byte deep)
+        {
+            undone_rounds =limit_rounds= deep;
+            while (undone_rounds > 0) Round(true);
+        }
+
+        /// <summary>
+        /// show hidden message
+        /// </summary>
+        public void Decrypt(byte deep)
+        {
+            undone_rounds =limit_rounds= deep;
+            while (undone_rounds > 0) Round(false);
+        }
+    }
+}
